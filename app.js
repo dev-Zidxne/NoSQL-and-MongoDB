e5cdb09443455daf726213c837af82ad877cbe72
Zidane Innis <zidaneinnis@gmail.com>
2023-04-28 16:27:06 -0500

    Creatin new users

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require("./controllers/error");
 
 const mongoConnect = require("./util/database").mongoConnect;
 
+const User = require("./models/user");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -19,12 +21,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then((user) => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
+  User.findById("644c39aea7edbf4e39ba0339")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
   next();
 });
 
