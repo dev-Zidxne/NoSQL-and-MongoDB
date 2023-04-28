caa20ed6bc4cef62173565d9ed1769f532fe5be7
Zidane Innis <zidaneinnis@gmail.com>
2023-04-28 18:58:32 -0500

    Working on cart items and orders

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const getDb = require("../util/database").getDb;
 
 const ObjectId = mongodb.ObjectId;
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
@@ -18,6 +20,21 @@ class User {
       .catch((err) => {
         console.log(err);
       });
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
