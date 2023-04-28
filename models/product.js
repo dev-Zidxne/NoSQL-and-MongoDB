09100fa82fe56ef8bd7593cd9c4c19cdce821314
Zidane Innis <zidaneinnis@gmail.com>
2023-04-28 15:05:08 -0500

    Working on the Product Model to Edit our Product

--- models/product.js
+++ models/product.js
@@ -3,15 +3,26 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
+    let dbOp;
+    if (this._id) {
+      dbOp = db;
+      // Update the product
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
     return db
       .collection("products")
       .insertOne(this)
@@ -34,7 +45,7 @@ class Product {
       .catch((err) => console.log(err));
   }
 
-  static findByPk(prodId) {
+  static findById(prodId) {
     const db = getDb();
     return db
       .collection("products")
