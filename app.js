94973bdfe167079016edf4373719615945aa44b9
Zidane Innis <zidaneinnis@gmail.com>
2023-04-28 19:27:16 -0500

    Adding the ADD-TO_CART functionality

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("644c39aea7edbf4e39ba0339")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
